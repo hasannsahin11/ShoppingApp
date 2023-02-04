da4e24c7692b0608f3667a92efc3cf398a0801d2
hasan <hasannsahin1@gmail.com>
2023-02-04 12:23:32 -0600

    Shopping classes update

--- src/RegularBuyer.java
+++ src/RegularBuyer.java
@@ -1,16 +1,33 @@
-public class RegularBuyer {
+public class RegularBuyer extends Buyer {
+
+    public RegularBuyer(String name, String address) {
+        super(name, address);
+    }
+
+    @Override
+    void checkOut(PaymentMethod paymentMethod) {
+
+    }
+
+    @Override
+    double calculateShippingCost() {
+        if (getCart().getTotal() < 50) {
+           return getCart().getTotal()*0.10;
+        }
+        return 0;
+    }
 
 
-   /** This class inherits from Buyer class. It has no additional variables
-    * Create a corresponding constructor to super.
-    *
-    * implement calculateShipping method. In the method
-    *                   if cart total is less than $50 return cartTotal*0.10
-    *
-    * Implement checkOut method. In the method
-    *                   call sellProduct method for each product and
-    *                   if payment method is CREDITCARD print "$cardTotal+shipping has been deducted from your card"
-    *                   if payment method is CASHonDELIVERY print "The amount you must pay on delivery is $cardTotal+shipping"
-    *
-    * */
+    /** This class inherits from Buyer class. It has no additional variables
+     * Create a corresponding constructor to super.
+     *
+     * implement calculateShipping method. In the method
+     *                   if cart total is less than $50 return cartTotal*0.10
+     *
+     * Implement checkOut method. In the method
+     *                   call sellProduct method for each product and
+     *                   if payment method is CREDITCARD print "$cardTotal+shipping has been deducted from your card"
+     *                   if payment method is CASHonDELIVERY print "The amount you must pay on delivery is $cardTotal+shipping"
+     *
+     * */
 }
--- src/RegularSeller.java
+++ src/RegularSeller.java
@@ -1,4 +1,17 @@
-public class RegularSeller {
+public class RegularSeller extends Seller{
+
+    public RegularSeller(String name, String address) {
+        super(name, address);
+        setFeeFromPerSale(0.10);
+        setUserType(UserType.REGULAR);
+
+    }
+
+    @Override
+    void sellProduct(Product product) {
+        setBalance(product.getPrice()-product.getPrice()*getFeeFromPerSale());
+        product.setStock(product.getStock()-1);
+    }
 
     /** This class inherits from Seller class. It has no additional variables.
      * Create a corresponding constructor with super and in the constructor
